09c4bcf305e83b1ca3ae83ea95694a75bdca9b8c
lolrew <111326707+lolrew@users.noreply.github.com>
2023-09-07 08:03:41 +0000

    hi

--- Objects/transaction/onlineCourses.py
+++ Objects/transaction/onlineCourses.py
@@ -1,8 +1,9 @@
 class onlineCourse:
-    def __init__(self, courseId, videos, price, image, studentPurchaseList, refundDescription, courseContent, requirements, description, courseForWho, instructor):
+    def __init__(self, courseId, name, videos, price, image, studentPurchaseList, refundDescription, courseContent, requirements, description, courseForWho, instructor):
         self.courseId = courseId
         self.videos = videos
         self.price = price
+        self.name = name
         self.image = image
         self.studentPurchaseList = studentPurchaseList if studentPurchaseList else []
         self.refundDescription = refundDescription
@@ -17,6 +18,7 @@ class onlineCourse:
             'courseId': self.courseId,
             'videos': self.videos,
             'price': self.price,
+            'name': self.name,
             'image': self.image,
             'studentPurchaseList': self.studentPurchaseList,
             'refundDescription': self.refundDescription,
--- __init__.py
+++ __init__.py
@@ -682,6 +682,7 @@ def course_admin():
                 "price": 15,
                 "studentPurchaseList": [],
                 "refundDescription": "30 days maximum",
+                'name': 'Duality of Body and Mind',
                 "courseContent": { 
 
                     'Basic Nutrition Module 1 - Getting started': {
@@ -783,6 +784,7 @@ def course_admin():
             course = onlineCourse(
                 data["courseId"],
                 data["videos"],
+                data["name"],
                 #data["createdBy"],
                 data["image"],
                 data["price"],
@@ -799,6 +801,7 @@ def course_admin():
             course = onlineCourse(
             data["courseId"],
             data["videos"],
+            data["name"],
             #data["createdBy"],
             data["image"],
             data["price"],
@@ -825,8 +828,8 @@ def course_admin():
         course_list.append(product)
     # prints out all the products and their info
     for course in course_list:
-        print(course.courseId, course.videos, course.price, course.studentPurchaseList, course.refundDescription, course.courseContent, course.requirements, course.description, course.courseForWho, course.instructor)
-        print("Course ID: ", course.courseId, "\n", "Product video: ", course.videos, "\n", "\n", "Course Price: ", course.price, "\n", "Student Purchase List: ", course.studentPurchaseList, "\n", "Refund Description: ", course.refundDescription, "\n", "Course Content: ", course.courseContent, "\n", "Requirements: ", course.requirements, "\n", "Description:", course.description, "\n", "Course For Who:", course.courseForWho, "\n", "Instructor: ", course.instructor)
+        print(course.courseId, course.videos, course.price, course.name, course.studentPurchaseList, course.refundDescription, course.courseContent, course.requirements, course.description, course.courseForWho, course.instructor)
+        print("Course ID: ", course.courseId, "\n", "Product video: ", course.videos, "\n", "\n", "Course Price: ", course.price, "\n", "Course Name", course.name, "\n", "Student Purchase List: ", course.studentPurchaseList, "\n", "Refund Description: ", course.refundDescription, "\n", "Course Content: ", course.courseContent, "\n", "Requirements: ", course.requirements, "\n", "Description:", course.description, "\n", "Course For Who:", course.courseForWho, "\n", "Instructor: ", course.instructor)
     return render_template('/Teachers/teacherLoggedInHome.html', course=course_list, count=len(course_list))
 
 
